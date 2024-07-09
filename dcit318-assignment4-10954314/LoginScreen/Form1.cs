be1278a097b5bf59a2221450873e3b462c4535e4
Akyere-Emmanuel-Kofi-10954314 <102963468+AkyereEmmanuelKofi@users.noreply.github.com>
2024-07-09 21:48:55 +0000

    Add files via upload

---
+++ dcit318-assignment4-10954314/LoginScreen/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Info");
+            UsernameTextBox.Clear();
+            PasswordTextBox.Clear();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
